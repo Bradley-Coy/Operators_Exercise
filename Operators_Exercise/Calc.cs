f5faa88d618e6c6010092f34fefa9642303f21fd
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-06 10:40:15 -0700

    first commit

---
+++ Operators_Exercise/Calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Operators_Exercise;
+    public class Calc
+    {
+        public static int Sum(int a, int b) {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b) {
+            return a - b;
+
+        }
+        public static int Mult(int a, int b) {
+            return a * b;
+
+        }
+        public static int Div(int a, int b) {
+            return a / b;
+
+        }
+  
+        public static int Mod(int a, int b) {
+            return a % b;
+
+        }
+
+        public void NewDiv(int a, int b) {
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{ a } / { b } is { quotient } remainder { remainder }");
+        }
+
+    }
+
+
+
---
+++ Operators_Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Operators_Exercise;
+
+Console.WriteLine("Please input two numbers");
+
+    int input1 = Convert.ToInt32(Console.ReadLine());
+
+    int input2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Select operation:\n" + "1 - Sum\n" + "2 - Subtract\n" + "3 - Multiply\n" + "4 - Divide\n" + "5 - Modulus\n" );
+
+    int selection = Convert.ToInt32(Console.ReadLine());
+
+switch (selection)
+{
+    case 1:
+        Console.WriteLine("The sum is equal to: " + Calc.Sum(input1, input2));
+        break;
+    case 2:
+        Console.WriteLine("The difference is equal to: " + Calc.Sub(input1, input2));
+        break;
+    case 3:
+        Console.WriteLine("The product is equal to:  " + Calc.Mult(input1, input2));
+        break;
+    case 4:
+        Console.WriteLine("The quotient is equal to: " + Calc.Div(input1, input2));
+        break;
+    case 5:
+        Console.WriteLine("The modulus is equal to: " + Calc.Mod(input1, input2));
+        break;
+}
+
+//Calc.NewDiv(17, 4);
+
+
